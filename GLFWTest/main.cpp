aa64658b946a0204f0463712c1757f0488cf4567
taskie <taskie@taskie-mba.local>
2012-11-28 01:24:41 +0900

    Initial Commit

---
+++ GLFWTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLFWTest
+//
+//  Created by taskie on 2012/11/28.
+//  Copyright (c) 2012年 alprosys. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
