ffbf1192df5422bb4b0b6dda2f48983b6620f111
taskie <taskie.aps@gmail.com>
2013-08-10 03:03:47 +0900

    rake

--- GLFWTest/rakefile.rb
+++ GLFWTest/rakefile.rb
@@ -2,14 +2,14 @@ require 'rake/clean'
 
 Compiler = 'g++'
 
-Out = 'glfwtest'
+Out = 'myshyp'
 task :default => Out
 
 Sources = FileList['**/*.cpp']
 Objects = Sources.ext('o')
 
-HeaderFlags = '-I/usr/local/include'
-LibraryFlags = '-static -lstdc++ -lmingw32 -m32 -mwindows -lm -lglfw -lopengl32 -lglu32 -llua -ltolua++ -lfreetype -lOpenAL32.dll -logg.dll -lvorbis.dll -lvorbisfile.dll'
+HeaderFlags = '-I/usr/local/include -I/usr/local/include/freetype'
+LibraryFlags = '-static -lstdc++ -lmingw32 -m32 -mwindows -lm -lglfw -lopengl32 -lglu32 -ltolua++ -lfreetype -lOpenAL32.dll -logg.dll -lvorbis.dll -lvorbisfile.dll lua51.dll'
 
 CLEAN.include(Objects)
 
@@ -21,5 +21,5 @@ file Out => Objects do |t|
 end
 
 rule '.o' => '.cpp' do |t|
-  sh "#{Compiler} -std=c++11 -Wall #{HeaderFlags} -DNDEBUG -c #{t.source} -o #{t.source.ext('o')}"
+  sh "#{Compiler} -std=c++11 -O3 -Wall #{HeaderFlags} -DNDEBUG -c #{t.source} -o #{t.source.ext('o')}"
 end
