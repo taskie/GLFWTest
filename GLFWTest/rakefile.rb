7ac1128a3e945d3b82be6bb551e1d4848db191f4
taskie <t@skie.jp>
2016-03-08 21:10:14 +0900

    for Arch Linux

--- GLFWTest/rakefile.rb
+++ GLFWTest/rakefile.rb
@@ -1,6 +1,6 @@
 require 'rake/clean'
 
-Compiler = 'icpc'
+Compiler = 'g++'
 
 Out = 'myshyp'
 task :default => Out
@@ -9,17 +9,14 @@ Sources = FileList['**/*.cpp']
 Objects = Sources.ext('o')
 
 HeaderFlags = '-I/usr/include -I/usr/include/freetype2 -I/usr/include/lua5.1'
-statLibraryFlags = '-lopenal -lGL -lGLU -lXrandr -lX11 -pthread -lm -lglfw -ltolua++5.1 -lfreetype -logg -lvorbis -lvorbisfile -lpng -lz -Bstatic -lluajit-5.1'
+LibraryFlags = '-lopenal -lGL -lGLU -lXrandr -lX11 -pthread -lm -lglfw -ltoluapp -lfreetype -logg -lvorbis -lvorbisfile -lpng -lz -Bstatic -lluajit-5.1'
 
 CLEAN.include(Objects)
 
 file Out => Objects do |t|
-  begin
-    sh "#{Compiler} #{t.prerequisites.join(' ')} #{LibraryFlags} -o #{Out}"
-  rescue
-  end
+  sh "#{Compiler} #{t.prerequisites.join(' ')} #{LibraryFlags} -o #{Out}"
 end
 
 rule '.o' => '.cpp' do |t|
-  sh "#{Compiler} -std=c++11 -O3 -xHOST -ip -fPIC -fma -no-prec-div -Wall #{HeaderFlags} -DNDEBUG -c #{t.source} -o #{t.source.ext('o')}"
+  sh "#{Compiler} -std=c++14 -O3 -Wall #{HeaderFlags} -DNDEBUG -c #{t.source} -o #{t.source.ext('o')}"
 end
